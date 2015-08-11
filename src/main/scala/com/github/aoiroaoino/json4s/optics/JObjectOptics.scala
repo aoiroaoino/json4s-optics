a4b968026751b1f3961942bfdd5cc1af93ac0141
aoino <aoiro.aoino+github@gmail.com>
2015-08-11 17:39:55 +0900

    add At instance for JObject

--- src/main/scala/com/github/aoiroaoino/json4s/optics/JObjectOptics.scala
+++ src/main/scala/com/github/aoiroaoino/json4s/optics/JObjectOptics.scala
@@ -1,6 +1,7 @@
 package com.github.aoiroaoino.json4s.optics
 
-import monocle.Prism
+import monocle.{Lens, Prism}
+import monocle.function.At
 import org.json4s.JsonAST.{JValue, JObject}
 
 trait JObjectOptics {
@@ -9,6 +10,15 @@ trait JObjectOptics {
     case jo: JObject => Some(jo.obj.toMap)
     case _           => None
   }(j => JObject(j.toList))
+
+  implicit def jObjectAt: At[JObject, String, JValue] = new At[JObject, String, JValue] {
+    def at(field: String) = Lens((_: JObject).obj.toMap.get(field)) {
+      optV => jObj => {
+        val map = jObj.obj.toMap
+        optV.fold(JObject(map - field toList))(v => JObject((map + (field -> v) toList)))
+      }
+    }
+  }
 }
 
 object JObjectOptics extends JObjectOptics
