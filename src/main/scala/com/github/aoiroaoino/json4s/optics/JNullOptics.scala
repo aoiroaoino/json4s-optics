2410ee0cd654d16403c49c8c4c27e793af9aa0ef
aoino <aoiro.aoino+github@gmail.com>
2015-09-02 03:26:14 +0900

    add optics for JNull

---
+++ src/main/scala/com/github/aoiroaoino/json4s/optics/JNullOptics.scala
@@ -0,0 +1,23 @@
+package com.github.aoiroaoino.json4s.optics
+
+import monocle.Prism
+import monocle.function.Empty
+
+import org.json4s.JsonAST.{JValue, JNull}
+
+trait JNullOptics {
+
+  def _jNull: Prism[JValue, JNull.type] =
+    jNullPrism
+
+  def jNullPrism = Prism[JValue, JNull.type] {
+    jValue => if (jValue == JNull) Some(JNull) else None
+  }(_ => JNull: JValue)
+
+  implicit def jNullEmpty: Empty[JNull.type] = new Empty[JNull.type] {
+    def empty: Prism[JNull.type, Unit] =
+      Prism[JNull.type, Unit](_ => Some(()))(_ => JNull)
+  }
+}
+
+object JNullOptics extends JNullOptics
